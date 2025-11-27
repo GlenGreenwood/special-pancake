633af974a25949aa9da4ed6bc53379fa2bf74e24
GlenGreenwood <wooing-gem-edge@duck.com>
2025-11-27 04:21:58 +0000

    Added JSON addition and updating.

--- functions.py
+++ functions.py
@@ -1,11 +1,17 @@
 import webbrowser
+import json
+# Establishing default bookmark groups and loading any existing bookmarks from a JSON file
 groups={"bookmarks1": ["https://docs.google.com/", "https://codecademy.com/","https://davinporter09.wixsite.com/shouldbelearning/"], "bookmarks2": ["https://www.khanacademy.com","https://classroom.google.com/","https://start.duckduckgo.com/"], "bookmarks3": ["https://www.coolmathgames.com/","https://www.example.com/","https://www.britannica.com/"]}
+with open('bookmarks.json', 'r') as json_file:
+    groups.update(json.load(json_file))
 
 def add_bookmark():
     choice = f"bookmarks{input('what set of bookmarks do you want to open? (1, 2, or 3)? ')}"
     new_bookmark = input("Enter the URL of the new bookmark: ")
     groups[choice].append(new_bookmark)
-        
+    with open('bookmarks.json', 'w') as json_file:
+        json.dump(groups, json_file, indent=4)
+
 def add_bookmark_loop():
     add_bookmark()
     while True:
@@ -18,6 +24,8 @@ def add_bookmark_loop():
 def addsection():
     key=f"bookmarks{len(groups)+1}"
     groups[key]=[input("Enter the URL of the a bookmark: ")]
+    with open('bookmarks.json', 'w') as json_file:
+        json.dump(groups, json_file, indent=4)
 
 
 def open_selection(select):
@@ -28,3 +36,4 @@ def open_selection(select):
 def choose_bookmarks():
     choice = f"bookmarks{input('what set of bookmarks do you want to open? (1, 2, or 3)? ')}"
     open_selection(groups[choice])
+
