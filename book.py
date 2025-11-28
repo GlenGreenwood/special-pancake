a0cf0b23e2c3fb5f570ebee8782f318a3eb777ae
GlenGreenwood <wooing-gem-edge@duck.com>
2025-11-28 22:06:11 +0000

    I'm not sure.

--- book.py
+++ book.py
@@ -1,6 +1,9 @@
 import webbrowser
 import json
 from flask import Flask, request, redirect
+from colorthief import ColorThief
+from user_agents import parse
+import random
 
 # Establishing default bookmark groups and loading any existing bookmarks from a JSON file
 def load_groups():
@@ -43,7 +46,42 @@ def open_selection(select):
         webbrowser.open(each)
     print("Good luck!")
 
+#this still has to be called from the console. I need to make a form for it.
 def choose_bookmarks():
     choice = f"bookmarks{input('what set of bookmarks do you want to open? (1, 2, or 3)? ')}"
     open_selection(groups[choice])
 
+def deviceType():
+    user_agent = request.headers.get('User-Agent')
+    parsed = parse(user_agent)
+    
+    if parsed.is_mobile:
+        device_type = 'Phone'
+    else:
+        device_type = 'Desktop'
+    return device_type
+
+def backgroundImage():
+    type=deviceType()
+    if type == 'Phone':
+        bgImage = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pixelstalk.net%2Fwp-content%2Fuploads%2F2016%2F07%2FDownload-Free-Pictures-3840x2160.jpg&f=1&nofb=1&ipt=832199581a53e02994f6809a03e36af683a87209e0e0a4ab3e9efad84a270390"
+    else:
+        bgImage = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pixelstalk.net%2Fwp-content%2Fuploads%2F2016%2F07%2FDownload-Free-Pictures-3840x2160.jpg&f=1&nofb=1&ipt=832199581a53e02994f6809a03e36af683a87209e0e0a4ab3e9efad84a270390"
+    return bgImage
+
+def colors():
+    bgImage = backgroundImage()
+    color_thief = ColorThief(bgImage)
+    colors=color_thief.get_palette(color_count=6)
+    ddgBackgroundcolor = "placeholder"
+    ddgTextcolor = "placeholder"
+    ddgLinkcolor = "placeholder"
+    ddgHeadercolor = "placeholder"
+    ddgURLcolor = "placeholder"
+    return colors
+print(colors())
+def SpirtualThought():
+    pass
+
+def waysToSayGL():
+    pass
