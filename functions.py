2c314705d4d45315a48b0a072a73faf88bce402b
GlenGreenwood <wooing-gem-edge@duck.com>
2025-11-28 00:41:33 +0000

    I changed the dictionary update into a function and added one case of error handling.

--- functions.py
+++ functions.py
@@ -1,12 +1,20 @@
 import webbrowser
 import json
-# Establishing default bookmark groups and loading any existing bookmarks from a JSON file
-groups={"bookmarks1": ["https://docs.google.com/", "https://codecademy.com/","https://davinporter09.wixsite.com/shouldbelearning/"], "bookmarks2": ["https://www.khanacademy.com","https://classroom.google.com/","https://start.duckduckgo.com/"], "bookmarks3": ["https://www.coolmathgames.com/","https://www.example.com/","https://www.britannica.com/"]}
-with open('bookmarks.json', 'r') as json_file:
-    groups.update(json.load(json_file))
 
+# Establishing default bookmark groups and loading any existing bookmarks from a JSON file
+def load_groups():
+    groups={"bookmarks1": ["https://docs.google.com/", "https://codecademy.com/","https://davinporter09.wixsite.com/shouldbelearning/"], "bookmarks2": ["https://www.khanacademy.com","https://classroom.google.com/","https://start.duckduckgo.com/"], "bookmarks3": ["https://www.coolmathgames.com/","https://www.example.com/","https://www.britannica.com/"]}
+    try:
+        with open('bookmarks.json', 'r') as json_file:
+            groups.update(json.load(json_file))
+    except:
+        pass
+    return groups
+
+groups=load_groups()
+#defines the function that adds a bookmark to the one of the selected sections. This funtion is called in many others.
 def add_bookmark():
-    choice = f"bookmarks{input('what set of bookmarks do you want to open? (1, 2, or 3)? ')}"
+    choice = f"bookmarks{input('what set of bookmarks do you want to add a bookmark to? (1, 2, or 3)? ')}"
     new_bookmark = input("Enter the URL of the new bookmark: ")
     groups[choice].append(new_bookmark)
     with open('bookmarks.json', 'w') as json_file:
